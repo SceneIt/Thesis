487b2ac260e0f85f10d08fc4e3464cc1b6399b03
Raymond Ma <raymondxma@gmail.com>
2014-12-19 15:14:57 -0800

    working on rendering map

--- client/app/map/MapController.js
+++ client/app/map/MapController.js
@@ -1,4 +1,16 @@
 angular.module('sceneit.map', [])
 
 .controller('MapController',function($scope) {
+    // Choose center and zoom level
+// var map = L.map('map').setView([51.505, -0.09], 13);
+
+// // add an OpenStreetMap tile layer
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// // add a marker in the given location, attach some popup content to it and open the popup
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+//     .openPopup();
 });
