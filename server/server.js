c3c98f3f1fde1a38e9b071d72e2b3b322d3a3ff2
Judy Zaratan <mochicat8@gmail.com>
2014-12-19 11:07:05 -0800

    Adds Files and updates Server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,15 @@
 var express        = require('express');
-var app            = express();
 var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client'))
-//console.log(__dirname + '/../client');
 
+var port = process.env.PORT || 8000;
+app.set('port', port);
 
-app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function(){
-  console.log("Applet listening on port " + process.env.PORT);
+  console.log("Applet listening on port " + port);
 });
 
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-app.use(express.static(__dirname + '/../client'));
-app.listen(8000,function() {
-	console.log('Listening on 8000');
-});
