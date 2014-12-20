d678915a0973acdc200aea720f70b9e01408e877
Judy Zaratan <mochicat8@gmail.com>
2014-12-19 16:16:32 -0800

    Fix map view

--- client/app/map/MapController.js
+++ client/app/map/MapController.js
@@ -1,14 +1,16 @@
 angular.module('sceneit.map', [])
 
 .controller('MapController',function($scope) {
-	var options = {
-    center: [40.4000, -3.6833], // Madrid
-    zoom: 7,
-    scrollwheel: true,
-    shareable: false
-	};
 
+    //loads the map onto the div
     var vizjson = 'http://secenit.cartodb.com/api/v2/viz/ba34720c-87c5-11e4-930e-0e853d047bba/viz.json';
     cartodb.createVis('map', vizjson, options);
     console.log('loaded');
+
+
+
+		var options = {
+			center: [40.4000, -3.6833] // Madrid
+		};
+
 });
