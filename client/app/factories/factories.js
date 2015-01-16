5296a1efde3a8cb7faa52a0953377f15bb94e708
mochicat8 <mochicat8@gmail.com>
2015-01-16 12:21:36 -0800

    Updates formatting

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -1,43 +1,43 @@
 angular.module('sceneit.factories', ['ngCookies'])
 
-.factory('Auth', function($state, $http, $window, $cookies){
+.factory('Auth', function($state, $http, $window, $cookies) {
 
   //User authenticated check
-  var isAuthenticated = function(){
-    if($cookies['userID']){
+  var isAuthenticated = function() {
+    if ($cookies['userID']) {
       return true;
     }
     return false;
   };
 
   //Signup post to server
-  var signup = function(user){
+  var signup = function(user) {
     return $http({
       method: 'POST',
       url: '/api/user/signup',
       data: user
-    }).then(function(res){
-        $state.go('home');
+    }).then(function(res) {
+      $state.go('home');
     });
   };
 
   //Signin post to server
-  var signin = function(user){
+  var signin = function(user) {
     return $http({
       method: 'POST',
       url: '/api/user/signin',
       data: user
-    }).then(function(res){
+    }).then(function(res) {
       $state.go('home');
     });
   };
 
   // Signout post to server
-  var signout = function(){
+  var signout = function() {
     return $http({
       method: 'POST',
       url: '/api/user/logout'
-    }).then(function(res){
+    }).then(function(res) {
       $state.go('signin');
     });
   };
@@ -46,6 +46,6 @@ angular.module('sceneit.factories', ['ngCookies'])
     signin: signin,
     signup: signup,
     signout: signout,
-    isAuthenticated: isAuthenticated 
+    isAuthenticated: isAuthenticated
   }
 });
