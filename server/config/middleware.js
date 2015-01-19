1cabf7fc975a6e9bf83a1f19f378a93479dff4e2
mochicat8 <mochicat8@gmail.com>
2015-01-19 11:45:44 -0800

    Comments and fix tabs spaces

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,48 +1,54 @@
 var bodyParser = require('body-parser');
 var utils = require('./utils'); // our custom middleware
-var passport   = require('passport');
-var session    = require('express-session');
-var morgan     = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+var morgan = require('morgan');
 var bcrypt = require('bcrypt-nodejs');
 var LocalStrategy = require('passport-local').Strategy;
 var db = require('../DB/db')
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 
 module.exports = function(app, express) {
-	var photoRouter = express.Router();
-	var commentRouter = express.Router();
+  var photoRouter = express.Router();
+  var commentRouter = express.Router();
+  var userRouter = express.Router();
 
-	app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
   app.use(bodyParser.json());
   app.use(allowCrossDomain);
-  app.use('/api/photo',photoRouter);
-  app.use('/photo', photoRouter);
-  app.use('/api/comments',commentRouter);
-
-  require('../photo/photoRoutes')(photoRouter);
-  require('../photo/takePhoto')(photoRouter);
-  require('../comment/commentRoutes')(commentRouter);
 
 
-  //Creates new user route
-  app.use(morgan('dev')); //Console log POST and GET status
+
+  //Logs POST and GET status
+  app.use(morgan('dev'));
 
   app.use(express.static(__dirname + '/../../client'))
 
- //required for passport --session secret
-  app.use(session({secret: 'mySecretKey', saveUninitialized: true, resave: false}));
-  app.use(passport.initialize());
-  app.use(passport.session()); //persistent login session
+  // Required for passport --session secret
+  app.use(session({
+    secret: 'mySecretKey',
+    saveUninitialized: true,
+    resave: false
+  }));
 
+  // Initialize passport and persistent login session
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Logs errors
   app.use(utils.errorLogger);
   app.use(utils.errorHandler);
 
+
+  //Passport middleware
   passport.serializeUser(function(user, done) {
     console.log('Serialized');
     done(null, user);
@@ -51,31 +57,39 @@ module.exports = function(app, express) {
   passport.deserializeUser(function(user, done) {
     console.log('Deserialized');
     db.User.find({
-      where:{
+      where: {
         userName: user.username
       }
-    }).then(function(user){
+    }).then(function(user) {
       done(null, user);
-    }).catch(function(err){
+    }).catch(function(err) {
       done(err, null);
     });
   });
 
 
-  var userRouter = express.Router();
-  app.use('/api/user', userRouter);
-  require('../user/userRoutes')(userRouter, passport);
-
+  // Use passport local strategy for signup
   passport.use('local-signup', new LocalStrategy({
-    passReqToCallback : true },
+      passReqToCallback: true
+    },
     utils.passportSignUp
-    ));
+  ));
 
-	passport.use('local-signin', new LocalStrategy({
-	  passReqToCallback: true },
+  // Use passport local strategy for signout 
+  passport.use('local-signin', new LocalStrategy({
+      passReqToCallback: true
+    },
     utils.passportSignIn
   ));
 
-};
+  app.use('/api/photo', photoRouter);
+  app.use('/api/comments', commentRouter);
+  app.use('/api/user', userRouter);
+  app.use('/photo', photoRouter);
 
+  require('../photo/photoRoutes')(photoRouter);
+  require('../photo/takePhoto')(photoRouter);
+  require('../comment/commentRoutes')(commentRouter);
+  require('../user/userRoutes')(userRouter, passport);
+};
 
